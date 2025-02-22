39207dce7d44b0a6c847f639d0b01f09a2536d4e
Sardor Aminov <dasturchioka@gmail.com>
2025-02-22 23:42:45 +0500

    logic changed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from 'express'
-import adminRoutes from './routes/admin.route'
 import dotenv from 'dotenv'
 import cors from 'cors'
 dotenv.config()
@@ -9,8 +8,6 @@ const port = process.env.PORT || 7667
 
 app.use(express.json())
 app.use(cors({ origin: '*' }))
-
-app.use('/api/v1/admin', adminRoutes)
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Express + TypeScript Server')
